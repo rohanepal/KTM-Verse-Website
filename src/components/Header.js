9c3855ee91878db347868fcf24e2de8386a00227
rohanepal <113876511+rohanepal@users.noreply.github.com>
2023-02-15 11:17:23 +0545

    progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { NavLink, Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header-top-strip py-3'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-6'>
+          <p className='text-white mb-0'>Free Shipping Over Rs.2000</p>
+          </div>
+          
+          <div className='col-6'>
+          <p className='text-end text-white mb-0'> 
+            Hotline: <a className='text-white' href="tel:+977 9742520951"> +977 9742520951</a></p>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
